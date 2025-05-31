c13619876e939bd3109e9b8dd070e8f928b7b212
Oluwatunmise Alimi <oluwatunmiselaimi67@gmail.com>
2025-05-31 12:31:01 +0100

    Clone

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'First Generative AI Project',
+    version= '0.1',
+    author= 'ALIMI Oluwatunmise',
+    author_email= 'oluwatunmisealimi67@gmail.com',
+    packages = find_packages(),
+    install_requires = []
+
+)
